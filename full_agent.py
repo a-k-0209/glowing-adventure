e60a372515b5e5386b9224c6b435abae06a916f0
a-k-0209 <anikakarampuri04@gmail.com>
2025-09-26 23:49:04 +0530

    Add files via upload

--- full_agent.py
+++ full_agent.py
@@ -1,19 +1,26 @@
-from langchain_openai import ChatOpenAI
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from calculator_tool import CalculatorTool
 from translator_tool import TranslatorTool
+import streamlit as st
 
-
-
+load_dotenv()
+st.set_page_config(page_title="Agent", page_icon="🤖")
 
 calculator = CalculatorTool()
 translator = TranslatorTool()
 
-
 tools = [calculator, translator]
 
-model = ChatOpenAI(model="gpt-4o-mini")
+API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    raise ValueError("API_KEY is not set. Please set it in your environment or .env file.")
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=API_KEY)
 
 
 prompt = ChatPromptTemplate.from_messages(
@@ -24,9 +31,9 @@ prompt = ChatPromptTemplate.from_messages(
         1. Always think step-by-step and explain in a clear numbered list.
         2. For "why" or "how" questions → provide a logical step-by-step explanation.
         3. For "which" or "what" questions → reason briefly, then give the final answer.
-        4. If the user asks for any mathematical calculations or mathematical questions, you must use the 'calculator' tool. 
+        4. If the user asks for any mathematical calculations or mathematical questions, you must use the 'calculator' tool and don't elaborate. 
         If the calculator tool cannot solve an math question, politely refuse.
-        5. If the user asks for any translation from one language to other, use the 'translator' tool.
+        5. If the user asks for any translation from one language to other, use the 'translator' tool and don't elaborate.
         If you cannot translate further using 'translator' tool, politely refuse
         6. Keep answers concise, structured, and easy to read.
 
@@ -40,56 +47,38 @@ prompt = ChatPromptTemplate.from_messages(
 agent = create_tool_calling_agent(model, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools)
 
-# agent_executor.invoke({"input": query})
-
-
-
-
-    # tools = load_tools("calculator", "translator", "wolframealpha")
-# prompt_template = PromptTemplate.from_template(
-#     f"""
-# You are a helpful AI agent. Follow these rules strictly:
-
-# 1. Always think step-by-step and explain in a clear numbered list.
-# 2. For "why" or "how" questions → provide a logical step-by-step explanation.
-# 3. For "which" or "what" questions → reason briefly, then give the final answer.
-# 4. If the user asks for any mathematical calculations or mathematical questions, you must use the 'calculator' tool. 
-# If the calculator tool cannot solve an math question, politely refuse.
-# 5. If the user asks for any translation from one language to other, use the 'translator' tool.
-# If you cannot translate further using 'translator' tool, politely refuse
-# 6. Keep answers concise, structured, and easy to read.
-
-# """)
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", "You are a helpful assistant. Respond only in Spanish."),
-#         ("human", "{input}"),
-#         # Placeholders fill up a **list** of messages
-#         ("placeholder", "{agent_scratchpad}"),
-#     ]
-# )
-
-# agent = initialize_agent(
-#     tools, llm, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
-#     )
 
 def main():
-    print("Level 3 Gemini Agent. Type 'exit' to exit chat.")
+    st.title("Level 3 Full Agent")
+     
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
 
-    while True:
-        user_input = input("You: ").strip()
+    for msg in st.session_state.chat_history:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
 
-        if not user_input:
-            continue
-        if user_input.lower() in {"exit", "quit"}:
-            print("Bye!")
-            break
+    user_input = st.chat_input("Your Message")
+
+    if user_input is not None and user_input != "":
+        st.session_state.chat_history.append({"role": "Human", "content": user_input})
+
+        with st.chat_message("Human"):
+            st.markdown(user_input)
 
         try:
-            response = agent_executor.invoke({'input': user_input})
-            print("AI:", response)
+            if user_input.lower() in {"exit", "quit"}:
+                response = "Bye!"
+            else:
+                response = agent_executor.invoke({'input': user_input})["output"]
         except ValueError:
-            print("The agent does not have a response. Please try again.")
+            response = "The agent does not have a response. Please try again."
+
+        st.session_state.chat_history.append({"role": "AI", "content": response})
+
+        with st.chat_message("AI"):
+            st.markdown(response)
 
 if __name__ == "__main__":
     main()
+
--- translator_tool.py
+++ translator_tool.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, Field
 from langchain.tools import StructuredTool
-from typing import Literal
+from googletrans import Translator, LANGUAGES
 
 class TranslatorInput(BaseModel):
     text: str = Field(..., description="input text to be translated")
-    target_language: Literal["German", "French", "Spanish"] = Field(..., description="Language to be translated to")
+    target_language: str = Field(..., description="Language to be translated to")
 
 class TranslatorTool(StructuredTool):
     name: str = "translator" 
@@ -12,13 +12,14 @@ class TranslatorTool(StructuredTool):
     args_schema: type[TranslatorInput] = TranslatorInput
 
     def _run(self, text: str, target_language: str):
-        translations = {
-            "Good Morning": {"German": "Guten Morgen", "French": "Bonjour", "Spanish": "Buenos días"},
-            "Have a nice day": {"German": "Einen schönen Tag", "French": "Bonne journée", "Spanish": "Que tengas un buen día"},
-            "Sunshine": {"German": "Sonnenschein", "French": "Rayon de soleil", "Spanish": "Luz del sol"}
-        }
+        # translations = {
+        #     "Good Morning": {"German": "Guten Morgen", "French": "Bonjour", "Spanish": "Buenos días"},
+        #     "Have a nice day": {"German": "Einen schönen Tag", "French": "Bonne journée", "Spanish": "Que tengas un buen día"},
+        #     "Sunshine": {"German": "Sonnenschein", "French": "Rayon de soleil", "Spanish": "Luz del sol"}
+        # }
+        dest_code = (code for code, name in LANGUAGES.items() if name == target_language.lower())
+        # detected_language = Translator.detect(text)
+        translator = Translator()
+        translation = translator.translate(text, dest="ja")
 
-        if text in translations and target_language in translations[text]:
-            return translations[text][target_language]
-        else:
-            return f"Translation not found"
+        return translation
